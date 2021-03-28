9379c18704283c5d949014616c12ddee9c347230
Ranjit Kalita <ranjitkalita499@gmail.com>
2021-03-29 01:07:31 +0530

    header

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <div>
+            <Container>
+                <HeaderLogo> 
+                    <h1>Local-E</h1>
+                </HeaderLogo>
+            </Container>
+        </div>
+    )
+}
+
+export default Header
+
+const Container=styled.div`
+height:60px;
+background-color:#4DA1A9;
+display:flex;
+align-items:center;
+`
+const HeaderLogo = styled.div`
+h1{
+    width:100px;
+    margin-left:10px;
+}
+`
+
